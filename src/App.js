6f8922d3bd42f5f13ebbcb65ab876d258ccc6f11
soonoo <qpseh2m7@gmail.com>
2018-05-15 01:37:33 +0900

    change. lift gallery's state to App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,29 @@ import FilterItem from './components/FilterItem';
 import Gallery from './components/Gallery';
 import './App.css';
 
+const CONTENTS_URL = "https://s3.ap-northeast-2.amazonaws.com/bucketplace-coding-test/feed/page_";
+
+const GalleryItemType = Object.freeze({
+  "All": "모두",
+  "Project": "집들이",
+  "Production": "제품",
+  "Card": "사진",
+  "Exhibition": "카드",
+});
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isFetchingImage: false,
+      page: 1,
+      galleryItems: [],
+      galleryItemType: GalleryItemType.All,
+    };
+    
+    this.loadImages = this.loadImages.bind(this);
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,15 +36,36 @@ class App extends Component {
           <LinkItem title={'북마크'} path={'/book-mark'} />
         </HeaderBar>
         <HeaderBar justify={'center'}>
-          <FilterItem title={'모두'} isEnabled/>
-          <FilterItem title={'사진'} />
-          <FilterItem title={'제품'} />
-          <FilterItem title={'집들이'} />
-          <FilterItem title={'기획전'} />
+          <FilterItem title={'모두'} isEnabled={} />
+          <FilterItem title={'사진'} isEnabled={} />
+          <FilterItem title={'제품'} isEnabled={} />
+          <FilterItem title={'집들이'} isEnabled={} />
+          <FilterItem title={'기획전'} isEnabled={} />
         </HeaderBar>
-        <Gallery />
+        <Gallery galleryItems={this.state.galleryItems}/>
       </div>
     );
+  }
+
+
+  async componentDidMount() {
+    this.loadImages();
+    window.addEventListener('scroll', this.loadImages);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.loadImages);
+  }
+
+  async loadImages() {
+    document.documentElement.scrollTop
+    const response = await fetch(`${CONTENTS_URL}${this.state.page}.json`);
+    const json = await response.json();
+
+    this.setState((prev) => ({
+      galleryItems: [...prev.galleryItems, ...json],
+      page: prev.page + 1,
+    }));
   }
 }
 
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,54 +4,21 @@ import React from 'react';
 import '../css/gallery.css';
 import GalleryItem from './GalleryItem';
 
-const CONTENTS_URL = "https://s3.ap-northeast-2.amazonaws.com/bucketplace-coding-test/feed/page_";
-
-export default class Gallery extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            isFetching: false,
-            page: 1,
-            galleryItems: [],
-        };
-
-        this.loadImages = this.loadImages.bind(this);
-    }
-
-    render() {
-        return (
-            <div className='gallery-wrapper'>
-                <ul>
-                    {this.state.galleryItems.map((item, index) => {
-                        return <GalleryItem
-                                key={index}
-                                url={item.image_url} 
-                                alt={item.title}
-                                type={item.type}
-                                />
-                    })}
-                </ul>
-            </div>
-        );
-    }
-
-    async componentDidMount() {
-        this.loadImages();
-        window.addEventListener('scroll', this.loadImages);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.loadImages);
-    }
-
-    async loadImages() {
-        document.documentElement.scrollTop
-        const response = await fetch(`${CONTENTS_URL}${this.state.page}.json`);
-        const json = await response.json();
-
-        this.setState((prev) => ({
-            galleryItems: [...prev.galleryItems, ...json],
-            page: prev.page + 1,
-        }));
-    }
+const Gallery = ({ galleryItems }) => {
+    return (
+        <div className='gallery-wrapper'>
+            <ul>
+                {galleryItems.map((item, index) => {
+                    return <GalleryItem
+                        key={index}
+                        url={item.image_url}
+                        alt={item.title}
+                        type={item.type}
+                    />
+                })}
+            </ul>
+        </div>
+    );
 }
+
+export default Gallery;
